7b3d5ce70544cb7823add73ef1f9acef1c8388b3
unknown <adrirf123@gmail.com>
2021-08-24 20:34:22 -0500

    Tarea1

---
+++ Javascript/Variables.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = -2;
+var c = -3;
+
+var resultado1 = (-b-(Math.sqrt(b**2-4*a*c)))/2*a
+var resultado2 = (-b+(Math.sqrt(b**2-4*a*c)))/2*a
+
+console.log("x1 es igual a :",resultado1)
+console.log("x2 es igual a: ",resultado2)
